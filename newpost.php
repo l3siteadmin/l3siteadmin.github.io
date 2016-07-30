71a3c2604d18ffc42d61eaf68b53f7c74667952c
l3siteadmin <ocs.social@aol.com>
2016-07-30 02:29:39 -0500

    Update newpost.php

--- newpost.php
+++ newpost.php
@@ -1,20 +1,6 @@
 <html>
   <body>
     Posting
-<?php
-$name = "/_posts/" + $_POST[datePost] + $_POST[namePost] + ".markup";
-$myfile = fopen($name, "w") or die("Unable to create file!");
-$txt = "---\n";
-fwrite($myfile, $txt);
-$txt = "layout: post\n";
-fwrite($myfile, $txt);
-$txt = "title:  \n" + $titlePost;
-fwrite($myfile, $txt);
-$txt = "---\n";
-fwrite($myfile, $txt);
-$txt = "---\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-?>
+
 </body>
 </html>
