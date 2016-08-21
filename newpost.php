b42681765450585a8b2a13270772fae1ba2f39fd
l3siteadmin <ocs.social@aol.com>
2016-08-21 16:23:27 -0500

    Update newpost.php

--- newpost.php
+++ newpost.php
@@ -2,18 +2,20 @@
   <body>
     Posting
 <?php
-$name = "/_posts/" + $_POST[datePost] + $_POST[namePost] + ".markup";
+$name = "/_posts/" + $_POST["datePost"] + $_POST["namePost"] + ".markup";
 $myfile = fopen($name, "w") or die("Unable to create file!");
 $txt = "---\n";
 fwrite($myfile, $txt);
-$txt = "layout: post\n";
-fwrite($myfile, $txt);
-$txt = "title:  \n" + $titlePost;
-fwrite($myfile, $txt);
-$txt = "---\n";
-fwrite($myfile, $txt);
-$txt = "---\n";
-fwrite($myfile, $txt);
+$txt1 = "layout: post\n";
+fwrite($myfile, $txt1);
+$txt2 = "title:  \n" + $_POST["titlePost"];
+fwrite($myfile, $txt2);
+$txt3 = "categories: jekyll update\n";
+fwrite($myfile, $txt3);
+$txt4 = "---\n";
+fwrite($myfile, $txt4);
+$txt4 = $_POST["messagePost"];
+fwrite($myfile, $txt5);
 fclose($myfile);
 ?>
 </body>
